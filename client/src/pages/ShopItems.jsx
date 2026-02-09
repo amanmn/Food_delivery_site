8fcd5812498b2b00c13ebf5795bd901d5c803a6a
Aman Porwal <amnmandliya786@gmail.com>
2026-02-09 12:33:24 +0530

    built shop api's

--- client/src/pages/ShopItems.jsx
+++ client/src/pages/ShopItems.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
 import { useGetItemsByShopQuery } from '../redux/features/product/itemApi'
 import { useEffect } from 'react'
 
 const ShopItems = () => {
-    const {data:items, isLoading, error} = useGetItemsByShopQuery();
-    
+    const { shopId } = useParams();
+    const { data: items, isLoading, error } = useGetItemsByShopQuery(shopId,
+        { skip: !shopId }
+    );
+
     useEffect(() => {
-        console.log("ShopItems component mounted,", items);
-    }, [items]);
+        console.log("shopId:", shopId);
+        console.log("items:", items);
+        console.log("isLoading:", isLoading);
+        console.log("error:", error);
+    }, [shopId, items, isLoading, error]);
+
+
     return (
         <div>
 
