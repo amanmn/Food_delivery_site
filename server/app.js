8a34072d9c5e731c99d683389cbfa79e5335d803
Aman Porwal <amnmandliya786@gmail.com>
2026-04-23 03:00:15 +0530

    updated Cors with frontend Url

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(
       "http://localhost:5173",
       process.env.FRONTEND_URL,
       "https://localhost:5173",
-      "https://food-delivery-site-opam.onrender.com",
+      "https://food-delivery-site-ebon.vercel.app",
     ],
     credentials: true,
   }));
